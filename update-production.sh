e6cdc6fd44d21f239e548028f3dad705d2fdd08b
Thomas Zahner <thomas.zahner@protonmail.ch>
2024-08-22 14:48:32 +0000

    Update script

--- update-production.sh
+++ update-production.sh
@@ -3,3 +3,8 @@
 git pull origin master --recurse-submodules
 git submodule update --init --recursive
 zola build
+
+# since zola temporarily deletes the directory mounted directory is no longer accessible from within running Docker containers.
+# maybe there could be a better work-around or fix?
+sudo docker compose down
+sudo docker compose up -d
